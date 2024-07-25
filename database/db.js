c57aeaf3beebc026813c668fc5e661e633534405
ZXdhruvZX <dhruvsharma7016@gmail.com>
2024-07-26 02:47:07 +0530

    first commit

--- database/db.js
+++ database/db.js
@@ -1,16 +1,15 @@
-// mongodb+srv://knupadhyay784:<password>@merndatabase.02svfwa.mongodb.net/
+import { connect } from 'mongoose';
+import dotenv from 'dotenv';
 
-import { connect } from 'mongoose'
-import 'dotenv/config';
+dotenv.config();
 
 const connectToMongo = async () => {
     try {
         await connect(process.env.MONGO_URI);
-        console.log("*** Database coonected Successfully ***");
+        console.log("*** Database connected Successfully ***");
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
+};
 
- 
 export default connectToMongo;
